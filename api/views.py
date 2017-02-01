76e0b5cf3d0f9d8ee31937cb95f0ec8d4f134a1f
Oswaldo <oswalfut_96@hotmail.com>
2017-02-01 13:23:20 -0600

    beginning orders

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from products.models import Producto
+from api.serializers import ProductSerializer
 
 # Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    
+    queryset = Producto.objects.all()
+    serializer_class = ProductSerializer
+    
+    
