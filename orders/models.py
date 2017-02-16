2c4235bc2210e8ce846ccce43570fda1233cc43a
DeividZavala <crusedmoss@gmail.com>
2017-02-16 13:31:13 -0600

    solving conflicts :hankey:

--- orders/models.py
+++ orders/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from products.models import Producto
 
+
 # Create your models here.
 class Order(models.Model):
-	user=models.OneToOneField(User)
-	date=models.DateField(auto_now=True)
-	products=models.ForeignKey(Producto)
-	total = models.DecimalField(decimal_places=2, max_digits=5)
-	address = models.TextField()
+    user = models.OneToOneField(User)
+    date = models.DateTimeField(auto_now=True, db_index=True)
+    products = models.ForeignKey(Producto)
+    total = models.DecimalField(decimal_places=2, max_digits=5)
+    address = models.TextField()
 
-	def __str__(self):
-		return self.id
-
+    def __str__(self):
+        return self.id
